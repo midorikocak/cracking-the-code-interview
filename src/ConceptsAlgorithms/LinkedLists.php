032fc49a5291b4574c0f22f04806dfac56da67df
Midori Kocak <Midori.Kocak@massiveinteractive.com>
2016-02-05 08:02:32 +0100

    linked lists 2.4 tested and solved

---
+++ src/ConceptsAlgorithms/LinkedLists.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace MidoriKocak\ConceptsAlgorithms;
+
+use Mtkocak\DataStructures;
+
+/**
+ * Class LinkedLists
+ * @package MidoriKocak\ConceptsAlgorithms
+ */
+class LinkedLists
+{
+    /**
+     * @param DataStructures\SingleLinkedListNode $node
+     */
+    public function deleteDups(DataStructures\SingleLinkedListNode &$node)
+    {
+        // In php arrays are hash tables
+        $table = [];
+        $previous = null;
+        while ($node != null) {
+            if (isset($table[$node->get()])) {
+                $previous->next($node->next());
+            } else {
+                $table[$node->get()] = true;
+                $previous = $node;
+            }
+            $node = $node->next();
+        }
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $node
+     */
+    public function deleteDups2(DataStructures\SingleLinkedListNode &$node)
+    {
+
+        if ($node == null) return;
+        $current = $node;
+
+        while ($current != null) {
+            $runner = $current;
+            while ($runner->next() != null) {
+                if ($runner->next()->get() == $current->get()) {
+                    $runner->next($runner->next()->next());
+                } else {
+                    $runner = $runner->next();
+                }
+            }
+            $current = $current->next();
+        }
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $head
+     * @param $size
+     * @return int
+     */
+    public function nthToLast(DataStructures\SingleLinkedListNode $head, $size)
+    {
+        if ($head == null) {
+            return 0;
+        }
+
+        $i = $this->nthToLast($head->next(), $size) + 1;
+        if ($i == $size) {
+            echo $head->get();
+        }
+        return $i;
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $head
+     * @param $k
+     * @param $i
+     * @return DataStructures\SingleLinkedListNode|null
+     */
+    public function nthToLast2(DataStructures\SingleLinkedListNode $head = null, $k, &$i)
+    {
+        if ($head == null) {
+            return null;
+        }
+        $node = $this->nthToLast2($head->next(), $k, $i);
+        $i++;
+        if ($k == $i) {
+            return $head;
+        }
+
+        return $node;
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $head
+     * @param $counter
+     * @return DataStructures\DataStructureNode|DataStructures\SingleLinkedListNode|null
+     */
+    public function nthToLast3(DataStructures\SingleLinkedListNode $head, $counter)
+    {
+        if ($counter <= null) {
+            return null;
+        }
+
+        $pointer1 = $head;
+        $pointer2 = $head;
+
+        // go $counter nodes first
+        for ($i = 0; $i < $counter - 1; $i++) {
+            $pointer2 = $pointer2->next();
+        }
+
+        if ($pointer2 == null) return null;
+
+        while ($pointer2->next() != null) {
+            $pointer1 = $pointer1->next();
+            $pointer2 = $pointer2->next();
+        }
+
+        return $pointer1;
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedList $list
+     * @return null
+     */
+    public function goToMiddleNode(DataStructures\SingleLinkedList &$list)
+    {
+        $pointer1 = $list->bottom;
+        $pointer2 = $list->bottom;
+        while ($pointer2->next() != null) {
+            $pointer1 = $pointer1->next();
+            $pointer2 = $pointer2->next()->next();
+        }
+        return $pointer1;
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $node
+     */
+    public function deleteNode(DataStructures\SingleLinkedListNode &$node)
+    {
+        if ($node == null || $node->next() == null) {
+            return false;
+        }
+        $next = $node->next();
+        $node->set($next->get());
+        $node->next($next->next());
+        unset($next);
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $node
+     * @param $x
+     * @return DataStructures\SingleLinkedListNode|null
+     */
+    public function partition(DataStructures\SingleLinkedListNode $node, $x)
+    {
+        $beforeStart = null;
+        $beforeEnd = null;
+        $afterStart = null;
+        $afterEnd = null;
+
+        while ($node != null) {
+            $next = $node->next();
+            $newNode = new DataStructures\SingleLinkedListNode($node->get());
+            if ($node->get() < $x) {
+                if ($beforeStart == null) {
+                    $beforeStart = $newNode;
+                    $beforeEnd = $beforeStart;
+                } else {
+                    $beforeEnd->next($newNode);
+                    $beforeEnd = $newNode;
+                }
+            } elseif ($node->get() > $x) {
+                if ($afterStart == null) {
+                    $afterStart = $newNode;
+                    $afterEnd = $newNode;
+                } else {
+                    $afterEnd->next($newNode);
+                    $afterEnd = $newNode;
+                }
+            } else {
+                $middleNode = $newNode;
+            }
+            $node = $next;
+        }
+
+        if ($beforeStart == null) {
+            return $afterStart;
+        }
+
+        $beforeEnd->next($middleNode);
+        $middleNode->next($afterStart);
+        return $beforeStart;
+    }
+
+    /**
+     * @param DataStructures\SingleLinkedListNode $node
+     * @param $x
+     * @return DataStructures\SingleLinkedListNode|null
+     */
+    public function partition2(DataStructures\SingleLinkedListNode $node, $x)
+    {
+        $beforeStart = null;
+        $afterStart = null;
+        while ($node != null) {
+            $next = $node->next();
+            $newNode = new DataStructures\SingleLinkedListNode($node->get());
+            if ($node->get() < $x) {
+                $newNode->next($beforeStart);
+                $beforeStart = $newNode;
+            } elseif ($node->get() > $x) {
+                $newNode->next($afterStart);
+                $afterStart = $newNode;
+            } else {
+                $middleNode = $newNode;
+            }
+            $node = $next;
+        }
+        //$beforeStart->next($middleNode);
+        $middleNode->next($afterStart);
+        if ($beforeStart == null) {
+            return $afterStart;
+        }
+        $head = $beforeStart;
+        while ($beforeStart->next() != null) {
+            $beforeStart = $beforeStart->next();
+        }
+        $beforeStart->next($middleNode);
+
+        return $head;
+    }
+}
---
+++ tests/LinkedListsTest.php
@@ -0,0 +1,168 @@
+<?php
+use MidoriKocak\ConceptsAlgorithms\LinkedLists;
+use Mtkocak\DataStructures;
+
+class LinkedListsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public $object;
+    public $linkedListWithDuplicate;
+    public $linkedListWithNoDuplicate;
+    public $linkedListTest;
+    public $middleDeleted;
+    public $unsorted;
+    public $partitioned;
+
+    public function setup()
+    {
+        $this->object = new LinkedLists();
+
+        $this->linkedListWithDuplicate = new DataStructures\SingleLinkedList();
+        $this->linkedListWithDuplicate->add("1");
+        $this->linkedListWithDuplicate->add("2");
+        $this->linkedListWithDuplicate->add("3");
+        $this->linkedListWithDuplicate->add("3");
+        $this->linkedListWithDuplicate->add("4");
+
+        $this->linkedListWithNoDuplicate = new DataStructures\SingleLinkedList();
+        $this->linkedListWithNoDuplicate->add("1");
+        $this->linkedListWithNoDuplicate->add("2");
+        $this->linkedListWithNoDuplicate->add("3");
+        $this->linkedListWithNoDuplicate->add("4");
+
+        $this->linkedListTest = new DataStructures\SingleLinkedList();
+        $this->linkedListTest->push("1");
+        $this->linkedListTest->push("2");
+        $this->linkedListTest->push("3");
+        $this->linkedListTest->push("4");
+        $this->linkedListTest->push("5");
+        $this->linkedListTest->push("6");
+        $this->linkedListTest->push("7");
+        $this->linkedListTest->push("8");
+        $this->linkedListTest->push("9");
+
+
+        $this->unsorted = new DataStructures\SingleLinkedList();
+        $this->unsorted->push("1");
+        $this->unsorted->push("6");
+        $this->unsorted->push("9");
+        $this->unsorted->push("8");
+        $this->unsorted->push("3");
+        $this->unsorted->push("2");
+        $this->unsorted->push("5");
+        $this->unsorted->push("7");
+        $this->unsorted->push("4");
+
+
+        $this->middleDeleted = new DataStructures\SingleLinkedList();
+        $this->middleDeleted->push("1");
+        $this->middleDeleted->push("2");
+        $this->middleDeleted->push("3");
+        $this->middleDeleted->push("4");
+        $this->middleDeleted->push("6");
+        $this->middleDeleted->push("7");
+        $this->middleDeleted->push("8");
+        $this->middleDeleted->push("9");
+
+        $this->partitioned = new DataStructures\SingleLinkedList();
+        $this->partitioned->push("1");
+        $this->partitioned->push("3");
+        $this->partitioned->push("2");
+        $this->partitioned->push("4");
+        $this->partitioned->push("5");
+        $this->partitioned->push("6");
+        $this->partitioned->push("9");
+        $this->partitioned->push("8");
+        $this->partitioned->push("7");
+
+        $this->partitioned2 = new DataStructures\SingleLinkedList();
+        $this->partitioned2->push("4");
+        $this->partitioned2->push("2");
+        $this->partitioned2->push("3");
+        $this->partitioned2->push("1");
+        $this->partitioned2->push("5");
+        $this->partitioned2->push("7");
+        $this->partitioned2->push("8");
+        $this->partitioned2->push("9");
+        $this->partitioned2->push("6");
+    }
+
+    /**
+     * 2.1 Write code to remove duplicates from an unsorted linked list.
+     * FOLLOW UP
+     * How would you solve this problem if a temporary buffer is not allowed?
+     */
+    public function testDeleteDups()
+    {
+        $node = $this->linkedListWithDuplicate->bottom;
+        $this->object->deleteDups($node);
+        //bottom of Linked lists gives us the linked list chain of elements only for comparation
+        $this->assertTrue($this->linkedListWithNoDuplicate->bottom == $this->linkedListWithDuplicate->bottom);
+    }
+
+    /**
+     * 2.1 Write code to remove duplicates from an unsorted linked list.
+     * FOLLOW UP
+     * How would you solve this problem if a temporary buffer is not allowed?
+     */
+    public function testDeleteDups2()
+    {
+        $node = $this->linkedListWithDuplicate->bottom;
+        $this->object->deleteDups2($node);
+        //bottom of Linked lists gives us the linked list chain of elements only for comparation
+        $this->assertTrue($this->linkedListWithNoDuplicate->bottom == $this->linkedListWithDuplicate->bottom);
+    }
+
+    /**
+     * 2.2 Implement an algorithm to find the kth to last element of a singly linked list.
+     */
+    public function testNthToLast()
+    {
+        $i = 0;
+        // $k  is element count???
+        $this->assertTrue($this->object->nthToLast2($this->linkedListTest->bottom, 3, $i)->get() == 7);
+    }
+
+    /**
+     * 2.2 Implement an algorithm to find the kth to last element of a singly linked list.
+     */
+    public function testNthToLast2()
+    {
+        $counter = 3;
+        $this->assertTrue($this->object->nthToLast3($this->linkedListTest->bottom, $counter)->get() == 7);
+    }
+
+    public function testGoToMiddleNode()
+    {
+        $this->assertTrue($this->object->goToMiddleNode($this->linkedListTest)->get() == 5);
+
+    }
+
+    /** 2.3 Implement an algorithm to delete a node in the middle of a singly linked list,
+     * given only access to that node.
+     */
+    public function testDeleteNode()
+    {
+        $middleNode = $this->object->goToMiddleNode($this->linkedListTest);
+        $this->object->deleteNode($middleNode);
+        $this->assertTrue($this->linkedListTest->bottom == $this->middleDeleted->bottom);
+    }
+
+    /**
+     * 2.4 Write code to partition a linked list around a value x, such that all nodes less than x
+     * come before alt nodes greater than or equal to x.
+     */
+    public function testPartition()
+    {
+        $this->assertTrue($this->object->partition($this->unsorted->bottom, 5) == $this->partitioned->bottom);
+    }
+
+    /**
+     * 2.4 Write code to partition a linked list around a value x, such that all nodes less than x
+     * come before alt nodes greater than or equal to x.
+     */
+    public function testPartition2()
+    {
+        $this->assertTrue($this->object->partition2($this->unsorted->bottom, 5) == $this->partitioned2->bottom);
+    }
+}
